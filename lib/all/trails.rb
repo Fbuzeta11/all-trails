4e8a00a553547aa107250d9fcc060ad98474e1ee
Federico Buzeta Borquez <fbuzeta11@gmail.com>
2020-05-12 00:10:25 +0000

    first commit

---
+++ lib/all/trails.rb
@@ -0,0 +1,8 @@
+require "all/trails/version"
+
+module All
+  module Trails
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/all/trails/version.rb
@@ -0,0 +1,5 @@
+module All
+  module Trails
+    VERSION = "0.1.0"
+  end
+end
